fc39ce1bd925ec6ea8680f0220ba6ca1fda902da
fersoftware <fersoftware@gmail.com>
2021-09-18 00:56:46 -0300

    add files

---
+++ s3-dio-live-datalake-bigdata-prod/temp/dio-live-wordcount-test.fersoft.20210918.023530.070506/files/wd/setup-wrapper.sh
@@ -0,0 +1,20 @@
+set -e
+
+# store $PWD
+__mrjob_PWD=$PWD
+
+# obtain exclusive file lock
+exec 9>/tmp/wrapper.lock.dio-live-wordcount-test.fersoft.20210918.023530.070506
+python3 -c 'import fcntl; fcntl.flock(9, fcntl.LOCK_EX)'
+
+# setup commands
+{
+  export PYTHONPATH=$__mrjob_PWD/mrjob.zip:$PYTHONPATH
+} 0</dev/null 1>&2
+
+# release exclusive file lock
+exec 9>&-
+
+# run task from the original working directory
+cd $__mrjob_PWD
+"$@"
